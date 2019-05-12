e92688bea2c5f1007b46b28ab5d63322e172fb4c
Simon Legg <leggsimon@gmail.com>
2019-05-12 12:56:44 +0100

    fetch open api document

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,67 @@
-import * as React from "react";
-import { render } from "react-dom";
+import * as React from 'react';
+import { render } from 'react-dom';
+import yaml from 'js-yaml';
+import { OpenAPIObject } from 'openapi3-ts';
 
-import "./styles.css";
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+interface AppProps {}
+
+interface AppState {
+	openApiDefinition: OpenAPIObject | undefined;
+	loadingState: 'loaded' | 'loading' | 'n/a';
 }
 
-const rootElement = document.getElementById("root");
+class App extends React.Component<AppProps, AppState> {
+	constructor(props: AppProps) {
+		super(props);
+
+		this.state = {
+			openApiDefinition: undefined,
+			loadingState: 'n/a',
+		};
+	}
+
+	async fetchDocument(url: string) {
+		const response = await fetch(url);
+		const text = await response.text();
+
+		this.setState({
+			loadingState: 'loaded',
+			openApiDefinition: yaml.safeLoad(text),
+		});
+	}
+
+	render() {
+		return (
+			<header className="App">
+				<h1>Hello Blah</h1>
+				<form
+					onSubmit={async event => {
+						event.preventDefault();
+						const { value } = document.getElementById('documentUrl') as HTMLInputElement;
+
+						await this.fetchDocument(value);
+
+						console.log('submitted', value);
+					}}
+				>
+					<label>
+						Import Document
+						<input
+							type="url"
+							name="url"
+							id="documentUrl"
+							defaultValue="https://raw.githubusercontent.com/OAI/OpenAPI-Specification/OpenAPI.next/examples/v3.0/petstore.yaml"
+						/>
+						<input type="submit" value="Import" />
+					</label>
+				</form>
+				{JSON.stringify(this.state.openApiDefinition, null, 2)}
+			</header>
+		);
+	}
+}
+
+const rootElement = document.getElementById('root');
 render(<App />, rootElement);
