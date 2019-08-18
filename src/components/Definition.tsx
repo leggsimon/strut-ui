6a42d8148f768b9ff7167f6aa24d670a13909910
Simon Legg <leggsimon@gmail.com>
2019-08-18 13:21:16 +0100

    render openapi metadata

--- src/components/Definition.tsx
+++ src/components/Definition.tsx
@@ -12,7 +12,52 @@ export function Definition({ openApiDefinition }: Props) {
 		<div>
 			<div className="metadata">
 				<h1>{openApiDefinition.info.title}</h1>
-				<span />
+				<p>{openApiDefinition.info.description}</p>
+				<details>
+					<summary>Addtional Information</summary>
+					<dl>
+						{
+							<>
+								<dt>Open Api Version</dt>
+								<dd>{openApiDefinition.openapi}</dd>
+							</>
+						}
+						{
+							<>
+								<dt>Api Version</dt>
+								<dd>{openApiDefinition.info.version}</dd>
+							</>
+						}
+						{openApiDefinition.info.termsOfService && (
+							<>
+								<dt>Terms Of Service</dt>
+								<dd>{openApiDefinition.info.termsOfService}</dd>
+							</>
+						)}
+						{openApiDefinition.info.contact && (
+							<>
+								<dt>Contact</dt>
+
+								<dd>
+									<dl>
+										<dt>Name</dt>
+										<dd>{openApiDefinition.info.contact.name}</dd>
+										<dt>Email</dt>
+										<dd>{openApiDefinition.info.contact.email}</dd>
+										<dt>URL</dt>
+										<dd>{openApiDefinition.info.contact.url}</dd>
+									</dl>
+								</dd>
+							</>
+						)}
+						{openApiDefinition.info.license && (
+							<>
+								<dt>Licence</dt>
+								<dd>{openApiDefinition.info.license}</dd>
+							</>
+						)}
+					</dl>
+				</details>
 			</div>
 			{JSON.stringify(openApiDefinition)}
 		</div>
